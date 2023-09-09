84979cd62497250221ba7b0fdb5b68d13ee08e37
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-09-09 19:36:47 +0300

    made changes on views

--- nexusbackend/meter_reading_api/views.py
+++ nexusbackend/meter_reading_api/views.py
@@ -19,3 +19,23 @@ class MeterReadingList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk, format=None):
+        try:
+            meter_reading = MeterReading.objects.get(pk=pk)
+        except MeterReading.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MeterReadingSerializer(meter_reading, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        try:
+            meter_reading = MeterReading.objects.get(pk=pk)
+        except MeterReading.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        meter_reading.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
