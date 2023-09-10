592e420eb645c9a260ea26f472686be827f6f34d
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-09-10 20:14:48 +0300

    make my return type more clear

--- nexusbackend/meter_reading/models.py
+++ nexusbackend/meter_reading/models.py
@@ -6,7 +6,8 @@ class MeterReading(models.Model):
     # meter = models.ForeignKey('Meter', on_delete=models.CASCADE) this has a relationship with the meter model
 
     def __str__(self):
-        return f"Meter Reading - {self.date}"
+     return f"Meter Reading - Date: {self.date}, Current Reading: {self.current_reading}"
+
 
 #class Meter(models.Model):
    # meter_number = models.IntegerField(unique=True)
