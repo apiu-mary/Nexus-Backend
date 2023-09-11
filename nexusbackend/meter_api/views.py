e160ab629b58c1652a042e489abc1dd2e74ed467
apiu-mary <apiuuagou@gmail.com>
2023-09-11 15:22:55 +0300

    added a delete and uodate methods

--- nexusbackend/meter_api/views.py
+++ nexusbackend/meter_api/views.py
@@ -1,12 +1,11 @@
-import statistics
-# from cairo import STATUS_CLIP_NOT_REPRESENTABLE, STATUS_DEVICE_ERROR, STATUS_DEVICE_FINISHED
+
 from django.shortcuts import render
-# from requests import Response
 from rest_framework.views import APIView
 from meter.models import Meter
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import MeterSerializer
+
+from meter_api.serializers import MeterSerializer
 
 class MeterListView(APIView):
     def get(self, request):
@@ -19,10 +18,31 @@ class MeterListView(APIView):
         serializer = MeterSerializer(meters, many=True)
         return Response(serializer.data)
 
-
     def post(self, request):
         serializer = MeterSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=STATUS_CLIP_NOT_REPRESENTABLE.HTTP_201_CREATED)
-        return Response(serializer.errors, status=STATUS_DEVICE_FINISHED.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            meter = Meter.objects.get(pk=pk)
+        except Meter.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        meter.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        try:
+            meter = Meter.objects.get(pk=pk)
+        except Meter.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MeterSerializer(meter, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
