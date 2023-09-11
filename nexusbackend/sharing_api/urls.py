56f90e9ba8f9eda69e1f0f7a5fd72a93c1c6026d
Moraaalice <alicemoraaongongo@gmail.com>
2023-09-11 10:13:57 +0300

    Updated my sahring_api

--- nexusbackend/sharing_api/urls.py
+++ nexusbackend/sharing_api/urls.py
@@ -1,7 +1,12 @@
+from django import views
 from django.urls import path
-from .views import UnitSharingList
+from .views import UnitSharingDetail, UnitSharingList
 
 urlpatterns = [
     path("sharing/" ,UnitSharingList.as_view(), name= "unit_sharing_list"),
+    path('sharing/<int:pk>/', UnitSharingDetail.as_view(), name='unit_sharing_detail'),
+    path('sharing_api/sharing/<int:pk>/', UnitSharingDetail.as_view(), name='unit_sharing_detail'),
+
+
 ]
 
--- nexusbackend/sharing_api/views.py
+++ nexusbackend/sharing_api/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,17 +7,50 @@ from .serializers import SharingSerializer
 from unit_sharing.models import UnitSharing
 # Create your views here.
 
+
 class UnitSharingList(APIView):
     def get(self,request):
         unit_sharing = UnitSharing.objects.all()
         serializer = SharingSerializer(unit_sharing,many=True)
         return Response(serializer.data)
-    
+     # The endpoint is supposed to to retrieve a list of all UnitSharing objects.
+    # Its typically used to fetch data from server,like displaying a list of shared units
+     
 
     def post(self,request):
         serializer = SharingSerializer(data=request.data)
-        if serializer.is_valid:
+        if serializer.is_valid():
+             serializer.save()
+             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+    # The post method is supposed to handle POST requests.This endpoint meant to create a new
+    # unit sharing object. Its used when a customer adds a new shared unit into the system
+
+
+class UnitSharingDetail(APIView):
+     def get_object(self, pk):
+        try:
+            return UnitSharing.objects.get(pk=pk)
+        except UnitSharing.DoesNotExist:
+            raise Http404
+        
+
+     def get(self, request, pk):
+        unit = self.get_object(pk)
+        serializer = SharingSerializer(unit)
+        return Response(serializer.data)
+     
+
+     def put(self, request, pk):
+        unit = self.get_object(pk)
+        serializer = SharingSerializer(unit, data=request.data)
+        if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-    
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     
+
+     def delete(self, request, pk):
+        unit = self.get_object(pk)
+        unit.delete()
+        return Response("Unit record deleted",status=status.HTTP_204_NO_CONTENT)
