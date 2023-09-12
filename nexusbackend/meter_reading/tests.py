49d2b7123f27f836bff3a350def86fa9c485595c
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-09-12 13:54:35 +0300

    Made changes to my model

--- nexusbackend/meter_reading/tests.py
+++ nexusbackend/meter_reading/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from meter_reading.models import MeterReading
 
 # Create your tests here.
+class MeterReadingTestCase(TestCase):
+    def setUp(self):
+        #  tests data for my model
+        self.reading1 = MeterReading.objects.create(current_reading=100.50, date='2023-09-12')
+        self.reading2 = MeterReading.objects.create(current_reading=150.75, date='2023-09-13')
+
+    def test_meter_reading_str_method(self):
+        # Tests the __str__ method of the MeterReading model
+        self.assertEqual(
+            str(self.reading1),
+            "Meter Reading - Date: 2023-09-12, Current Reading: 100.50"
+        )
+        self.assertEqual(
+            str(self.reading2),
+            "Meter Reading - Date: 2023-09-13, Current Reading: 150.75"
+        )
