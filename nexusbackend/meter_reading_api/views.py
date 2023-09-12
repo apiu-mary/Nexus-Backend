ea1699fc16b009827a63144aaf33fb450faae7d1
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-09-12 11:02:48 +0300

    Made changes to my model

--- nexusbackend/meter_reading_api/views.py
+++ nexusbackend/meter_reading_api/views.py
@@ -1,28 +1,24 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from meter_reading.models import MeterReading
 from .serializers import MeterReadingSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-# Create your views here.
 
 class MeterReadingList(APIView):
-    def get_object(self, pk):
-        try:
-            return MeterReading.objects.get(pk=pk)
-        except MeterReading.DoesNotExist:
-            raise Http404
-
     def get(self, request, pk=None, format=None):
-        if pk is not None:
-            meter_reading = self.get_object(pk)
-            serializer = MeterReadingSerializer(meter_reading)
-            return Response(serializer.data)
-        else:
-            meter_readings = MeterReading.objects.all()
-            serializer = MeterReadingSerializer(meter_readings, many=True)
-            return Response(serializer.data)
+        try:
+            if pk is not None:
+                meter_reading = get_object_or_404(MeterReading, pk=pk)
+                serializer = MeterReadingSerializer(meter_reading)
+                return Response(serializer.data)
+            else:
+                meter_readings = MeterReading.objects.all()
+                serializer = MeterReadingSerializer(meter_readings, many=True)
+                return Response(serializer.data)
+        except MeterReading.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
 
     def post(self, request, format=None):
         serializer = MeterReadingSerializer(data=request.data)
@@ -31,23 +27,23 @@ class MeterReadingList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
     def put(self, request, pk, format=None):
         try:
             meter_reading = MeterReading.objects.get(pk=pk)
+            serializer = MeterReadingSerializer(meter_reading, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except MeterReading.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = MeterReadingSerializer(meter_reading, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
         try:
             meter_reading = MeterReading.objects.get(pk=pk)
+            meter_reading.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
         except MeterReading.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-        meter_reading.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
