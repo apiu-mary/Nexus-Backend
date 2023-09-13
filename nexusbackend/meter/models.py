88a80826dcfabae4392c2dc1723a1685fc8f0d35
apiu-mary <apiuuagou@gmail.com>
2023-09-13 16:28:07 +0300

    added more tests

--- nexusbackend/meter/models.py
+++ nexusbackend/meter/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 class Meter(models.Model):
     meter_serial_number = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    current_reading = models.PositiveIntegerField()
+    current_reading = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=32)
     
     def __str__(self):
-        return self.current_reading
-
-
+        return f"{self.current_reading} A"
+    
+   
+   
 
 
 
--- nexusbackend/meter_api/tests.py
+++ nexusbackend/meter_api/tests.py
@@ -1,3 +1,56 @@
+import uuid
+from decimal import Decimal
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from meter.models import Meter
 
-# Create your tests here.
+class MeterTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.meter_data = {
+            "current_reading": "50.0",
+            "status": "active"
+        }
+        self.meter = Meter.objects.create(**self.meter_data)
+        self.url_list = reverse("MeterListView")
+        self.url_detail = reverse("MeterDetailsview", args=[self.meter.pk])
+
+    def test_list_meters(self):
+        response = self.client.get(self.url_list)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_create_meter(self):
+        response = self.client.post(self.url_list, self.meter_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Meter.objects.count(), 2)
+
+    def test_get_meter_details(self):
+        response = self.client.get(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  
+
+    def test_update_meter_details(self):
+        updated_data = {
+            "current_reading": "60.0",
+            "status": "inactive"
+        }
+        response = self.client.put(self.url_detail, updated_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.meter.refresh_from_db()
+        self.assertEqual(float(self.meter.current_reading), 60.0)
+
+    def test_update_nonexistent_meter_details(self):
+        nonexistent_url = reverse("MeterDetailsview", args=[999])
+        response = self.client.put(nonexistent_url, self.meter_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_meter(self):
+        response = self.client.delete(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Meter.objects.count(), 0)
+
+ 
