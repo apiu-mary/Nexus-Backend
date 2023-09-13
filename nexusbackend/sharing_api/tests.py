20bdf2081533f1be4a15f57a05dca670b7b7e713
Moraaalice <alicemoraaongongo@gmail.com>
2023-09-13 08:20:04 +0300

    Modified my APIs to remove the DELETE method

--- nexusbackend/sharing_api/tests.py
+++ nexusbackend/sharing_api/tests.py
@@ -1,2 +1,41 @@
 from django.test import TestCase
+from django.utils import timezone
+from unit_sharing.models import UnitSharing
 # Create your tests here.
+
+class UnitSharingModelTestCase(TestCase):
+    def test_model_creation(self):
+        shared_units = 10.5
+        created_at = timezone.now()
+        updated_at = timezone.now()
+        
+        unit_sharing = UnitSharing.objects.create(
+            shared_units=shared_units,
+            created_at=created_at,
+            updated_at=updated_at,
+        )
+        
+        self.assertIsInstance(unit_sharing, UnitSharing)
+        self.assertEqual(unit_sharing.shared_units, shared_units)
+        self.assertEqual(unit_sharing.created_at, created_at)
+        self.assertEqual(unit_sharing.updated_at, updated_at)
+
+    def test_model_str_representation(self):
+        shared_units = 7.25
+        created_at = timezone.now()
+        updated_at = timezone.now()
+
+        unit_sharing = UnitSharing(
+            shared_units=shared_units,
+            created_at=created_at,
+            updated_at=updated_at,
+        )
+        expected_str = f"Shared Units: {shared_units}, Created At: {created_at}, Updated At: {updated_at}"
+        self.assertEqual(str(unit_sharing), expected_str)
+
+
+    def test_max_digits_constraint(self):
+        with self.assertRaises(Exception) as context:
+            UnitSharing.objects.create(shared_units=12345.6789)
+        self.assertIn(str(context.exception))
+
--- nexusbackend/sharing_api/views.py
+++ nexusbackend/sharing_api/views.py
@@ -6,7 +6,6 @@ from .serializers import SharingSerializer
 from unit_sharing.models import UnitSharing
 # Create your views here.
 
-
 @api_view(['GET','POST'])
 def sharing_list(request):
     if request.method =='GET':
@@ -18,12 +17,12 @@ def sharing_list(request):
        if serializer.is_valid():
           serializer.save()
           return Response(serializer.data, status=status.HTTP_201_CREATED)
-       
 
-@api_view(['GET','PUT','DELETE'])
+@api_view(['GET', 'PUT'])
 def sharing_detail(request, id):
     try:
         sharing = UnitSharing.objects.get(pk=id)
+
         if request.method == 'GET':
             serializer = SharingSerializer(sharing)
             return Response(serializer.data)
@@ -33,7 +32,7 @@ def sharing_detail(request, id):
                 serializer.save()
                 return Response(
                     {
-                        "message": "Units successfully shared",
+                        "message": "Units successfully edited",
                         "device_data": serializer.data
                     }
                 )
@@ -46,57 +45,9 @@ def sharing_detail(request, id):
                     },
                     status=status.HTTP_400_BAD_REQUEST
                 )
-        elif request.method == 'DELETE':
-            sharing.delete()
-            return Response({"message": "Shared units  deleted"}, status=status.HTTP_204_NO_CONTENT)
     except UnitSharing.DoesNotExist:
-        return Response({"error": "Units not found", "message": "The requested shared units does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"error": "Units not found", "message": "The requested shared units do not exist"},
+            status=status.HTTP_404_NOT_FOUND
+        )
 
-
-# class UnitSharingList(APIView):
-
-#     def get(self,request):
-#         unit_sharing = UnitSharing.objects.all()
-#         serializer = SharingSerializer(unit_sharing,many=True)
-#         return Response(serializer.data)
-#      # The endpoint is supposed to to retrieve a list of all UnitSharing objects.
-#     # Its typically used to fetch data from server,like displaying a list of shared units
-     
-
-#     def post(self,request):
-#         serializer = SharingSerializer(data=request.data)
-#         if serializer.is_valid():
-#              serializer.save()
-#              return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-#     # The post method is supposed to handle POST requests.This endpoint meant to create a new
-#     # unit sharing object. Its used when a customer adds a new shared unit into the system
-
-
-# class UnitSharingDetail(APIView):
-#      def get_object(self, pk):
-#         try:
-#             return UnitSharing.objects.get(pk=pk)
-#         except UnitSharing.DoesNotExist:
-#             raise Http404
-        
-
-#      def get(self, request, pk):
-#         unit = self.get_object(pk)
-#         serializer = SharingSerializer(unit)
-#         return Response(serializer.data)
-     
-
-#      def put(self, request, pk):
-#         unit = self.get_object(pk)
-#         serializer = SharingSerializer(unit, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-     
-
-#      def delete(self, request, pk):
-#         unit = self.get_object(pk)
-#         unit.delete()
-#         return Response("Unit record deleted",status=status.HTTP_204_NO_CONTENT)
