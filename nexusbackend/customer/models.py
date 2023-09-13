9508da2a6a044ad8c07613257c1a249bbbf80f1c
MariaGKimani <mariakimanigoretti@gmail.com>
2023-09-13 09:13:58 +0300

    added the test for customer model

--- nexusbackend/customer/models.py
+++ nexusbackend/customer/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
 
 class Customer(models.Model):
     # user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
     # meter = models.OneToOneField(Meter, on_delete=models.CASCADE)  
     # units = models.ForeignKey(Units, on_delete=models.CASCADE)  
-    # meter_reading = models.OneToOneField(MeterReading, on_delete=models.CASCADE)
+    # meter_reading = models.OneToOneField(MeterReading, 
+    # on_delete=models.CASCADE)
     company_name = models.CharField(max_length=32)
     country = models.CharField(max_length=32)
     city = models.CharField(max_length=64, default=None)
--- nexusbackend/customer/tests.py
+++ nexusbackend/customer/tests.py
@@ -1,21 +1,28 @@
 from django.test import TestCase
 from customer.models import Customer
 
+
 class UserModelTest(TestCase):
     def setUp(self):
         self.user = Customer.objects.create(
             company_name='Nexus',
-            phone='+123456789',
-            city ='123 Main St',
+            phone_number='+123456789',
+            city='123 Main St',
             country='password'
         )
+        
     def test_user_creation(self):
         
-        user = Customer.objects.get(username='Nexus')
+        user = Customer.objects.get(company_name='Nexus')
        
-        self.assertEqual(user.phone, '+123456789')
+        self.assertEqual(user.phone_number, '+123456789')
+        
         self.assertEqual(user.city, '123 Main St')
-        self.assertEqual(user.company_name, 'password')
+        self.assertEqual(user.country, 'password')
+             
     def test_user_str_representation(self):
         
-        self.assertEqual(str(self.user), 'Nexus') 
+        self.assertEqual(str(self.user), 'Nexus')
+      
+    def tearDown(self):
+        self.user.delete()
