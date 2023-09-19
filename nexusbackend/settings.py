e7a0ae9ac03939f6f243e4b391d04eb006fbd3e8
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-09-19 21:41:19 +0300

    made changes to the imports

--- nexusbackend/settings.py
+++ nexusbackend/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url
 from pathlib import Path
-import environ
 
 
-env = environ.Env()
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
