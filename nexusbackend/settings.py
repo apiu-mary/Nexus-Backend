db0c7c00f53f09c05cea70a8e3e01e66fc52466a
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-09-20 10:45:40 +0300

    made changes to the imports

--- nexusbackend/settings.py
+++ nexusbackend/settings.py
@@ -11,13 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url
 from pathlib import Path
-
-from dotenv import load_dotenv
-load_dotenv()
-
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
