f30696032f2be797a6f76045aa551c387fc75a64
apiu-mary <apiuuagou@gmail.com>
2023-11-23 08:10:10 +0300

    created the backend project for the Nexus Interface unit project

--- meter_reading/models.py
+++ meter_reading/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from meter.models import Meter
 
 class MeterReading(models.Model):
-    meter = models.ForeignKey(Meter, on_delete=models.CASCADE, related_name='readings', null=True)
+    # meter = models.ForeignKey(Meter, on_delete=models.CASCADE, related_name='readings', null=True)
     current_reading = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(null=True)
     
